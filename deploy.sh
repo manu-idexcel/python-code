426ecbaae1aa2026a216d8f570c8ab970be5b2bf
manu-idexcel <55433054+manu-idexcel@users.noreply.github.com>
2021-12-27 12:46:12 +0530

    updated scripts

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "write text to file"
+echo "Hello World" > /tmp/hello.txt
+echo "sleep 60 seconds"
+sleep 60
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "starting python server"
+python -m SimpleHTTPServer 8069
